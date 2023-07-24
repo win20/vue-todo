6f940aaef78e29061a6d4c9eec69d8e4634bba88
Win Barua <win.barua2@gmail.com>
2023-07-24 23:02:46 +0100

    Todo styling and default message

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue';
 import { uid } from 'uid';
+import { Icon } from '@iconify/vue';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
 
 const todoList = ref([]);
 
@@ -19,6 +21,13 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete!</span>
+    </p>
   </main>
 </template>
 
@@ -34,6 +43,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
