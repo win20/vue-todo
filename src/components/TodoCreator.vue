bf6e3830d977f88857387dfb18e2c5bd14242853
Win Barua <win.barua2@gmail.com>
2023-07-24 22:17:24 +0100

    Add create todo text box and button

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+const emit = defineEmits(["createTodo"]);
+const todoState = reactive({
+  todo: "",
+  invalid: null,
+  errMsg: "",
+});
+
+const createTodo = () => {
+  emit('createTodo', todoState.todo);
+};
+</script>
+
+<template>
+  <div class="input-wrap">
+    <input type="text" v-model="todoState.todo" />
+    <button @click="createTodo()">Create</button>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
